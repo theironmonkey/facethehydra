8c4b6cb4a2a582be45ca757b4a155c16ab0b0dec
theironmonkey <67200657+theironmonkey@users.noreply.github.com>
2025-07-07 12:38:57 -0700

    Delete assets directory

--- assets/index-def456.js
+++
@@ -1,6 +0,0 @@
-
-import { createRoot } from "https://esm.sh/react-dom@18/client";
-import React from "https://esm.sh/react";
-
-const App = () => React.createElement('h1', null, 'Face the Hydra Tracker (Vite Build)');
-createRoot(document.getElementById("root")).render(React.createElement(App));
