0cfa33b0924da4c01a7c08498a28221f09f02f19
theironmonkey <67200657+theironmonkey@users.noreply.github.com>
2025-07-07 10:45:34 -0700

    Add files via upload

---
+++ assets/index-def456.js
@@ -0,0 +1,6 @@
+
+import { createRoot } from "https://esm.sh/react-dom@18/client";
+import React from "https://esm.sh/react";
+
+const App = () => React.createElement('h1', null, 'Face the Hydra Tracker (Vite Build)');
+createRoot(document.getElementById("root")).render(React.createElement(App));
